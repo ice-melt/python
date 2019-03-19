fcb370e451cf29cbec221a881249507b9f8cc02f
ice-melt <ice-melt@outlook.com>
2019-03-19 15:35:13 +0800

    update code:model of utiles

--- modules/utils/serialize.py
+++ modules/utils/serialize.py
@@ -2,11 +2,11 @@ import pickle
 
 
 # 序列化
-def serialize(fileName,data):
-    with open('%s.pkl'%fileName, 'wb') as f:
+def serialize(filename, data):
+    with open('%s.pkl' % filename, 'wb') as f:
         pickle.dump(data, f)
 
 # 反序列化
-def unserialize(fileName):
-    with open('%s.pkl'%fileName, 'rb') as f:
+def unserialize(filename):
+    with open(filename, 'rb') as f:
         return pickle.load(f)
